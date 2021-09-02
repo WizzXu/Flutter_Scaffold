d0d4bb51727c8c27591c324f808fabc687656b3a
xuweiyu <wizz.xu@outlook.com>
2021-09-02 22:38:21 +0800

    添加jni从java获取byte数组

--- plugins/JNIDemo/app/src/main/cpp/native-lib.cpp
+++ plugins/JNIDemo/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,30 @@
 #include <jni.h>
 #include <string>
+#include<android/log.h>
 
-extern "C" JNIEXPORT jbyteArray JNICALL
-Java_com_example_jnidemo_MainActivity_bytesFromJNI(JNIEnv *env, jobject thiz) {
-    jbyte a[] = {1,2,3,4,5,6};
+#define TAG "myDemo-jni" // 这个是自定义的LOG的标识
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__) // 定义LOGD类型
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG ,__VA_ARGS__) // 定义LOGI类型
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,TAG ,__VA_ARGS__) // 定义LOGW类型
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__) // 定义LOGE类型
+#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,TAG ,__VA_ARGS__) // 定义LOGF类型
+
+
+extern "C"
+JNIEXPORT jbyteArray JNICALL
+Java_com_example_jnidemo_MainActivity_getBytesFromJNI(JNIEnv *env, jobject thiz,
+                                                   jbyteArray byte_array) {
+    jint byte_array_size = env->GetArrayLength(byte_array);
+    jbyte *p_byte_array = (*env).GetByteArrayElements(byte_array, nullptr);
+
+    int i;
+    for (i = 0; i < byte_array_size; i++)
+        LOGI("[%d]", *(p_byte_array + i));
+
+    env->ReleaseByteArrayElements(byte_array, p_byte_array, NULL);
+    jbyte ret_a[] = {5,5,5,5,5,5};
+
     jbyteArray ret = env->NewByteArray(6);
-    env->SetByteArrayRegion (ret, 0, 6, a);
+    env->SetByteArrayRegion (ret, 0, 6, ret_a);
     return ret;
 }
--- plugins/JNIDemo/app/src/main/java/com/example/jnidemo/MainActivity.kt
+++ plugins/JNIDemo/app/src/main/java/com/example/jnidemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.jnidemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import com.example.jnidemo.databinding.ActivityMainBinding
 
@@ -16,14 +17,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // Example of a call to a native method
-        binding.sampleText.text = bytesFromJNI().toUByteArray().toString()
+        binding.sampleText.text = getBytesFromJNI(byteArrayOf(1,2,3,4,5,6)).toUByteArray().toString()
+        //val byteArray:ByteArray = getBytesFromJNI(byteArrayOf(1,2,3,4,5,6))
+    }
+
+    fun getBytes(byteArray: ByteArray):ByteArray{
+        Log.e("获取byteArray，入参为：", byteArray.toUByteArray().toString())
+        return byteArrayOf(1,2,3,4,5,6)
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    external fun bytesFromJNI(): ByteArray
+    private external fun getBytesFromJNI(byteArray: ByteArray): ByteArray
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
